55b9c5442c778d71c69b28dfe10bc206a6dd428d
hkxiaoyu <hkxiaoyu118@gmail.com>
2019-04-26 14:47:00 +0800

    添加了获取进程启动时间的函数

--- userbase/UserTimer.cpp
+++ userbase/UserTimer.cpp
@@ -3,9 +3,28 @@
 
 namespace ubase
 {
-	__int64 TimerGetUnixTimeNow()
+	__int64 TimerGetUnixTime()
 	{
 		time_t t = time(0);
 		return t;
+	}
+
+	__int64 TimerGetProcessCreateTime(DWORD dwPid)
+	{
+		LONGLONG llCreateTime = 0;
+		HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, dwPid);
+		if (hProcess != NULL)
+		{
+			FILETIME ftCreation, ftExit, ftKernel, ftUser;
+			if (GetProcessTimes(hProcess, &ftCreation, &ftExit, &ftKernel, &ftUser) == TRUE)
+			{
+				ULARGE_INTEGER ui;
+				ui.LowPart = ftCreation.dwLowDateTime;
+				ui.HighPart = ftCreation.dwHighDateTime;
+				llCreateTime = ((LONGLONG)(ui.QuadPart - 116444736000000000) / 10000000);
+			}
+			CloseHandle(hProcess);
+		}
+		return llCreateTime;
 	}
 }
--- userbase/UserTimer.h
+++ userbase/UserTimer.h
@@ -3,5 +3,6 @@
 
 namespace ubase
 {
-	__int64 TimerGetUnixTimeNow();
+	__int64 TimerGetUnixTime();//获取当前的UNIX时间戳
+	__int64 TimerGetProcessCreateTime(DWORD dwPid);//获取进程的启动时间
 }
